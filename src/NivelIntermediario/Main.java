124f0299c68cb9e6d4e3972eddd2e90c6b55cd08
Danillo araujo <danillo.schwager06@gmail.com>
2026-01-19 16:13:19 -0300

    Initial commit

---
+++ src/NivelIntermediario/Main.java
@@ -0,0 +1,31 @@
+package NivelIntermediario;
+
+import java.util.Scanner;
+
+public class Main {
+     public static void main(String[] args) {
+
+         //Criar o ninja Naruto é um OBJETO
+
+         Ninja Naruto = new Ninja();
+         Naruto.nome = "Naruto Uzumaki";
+         Naruto.idade = 19;
+         Naruto.aldeia = "Aldeia da folha";
+
+         //Criar o Ninja Sasuke é um OBJETO
+
+         Ninja Sasuke = new Ninja();
+         Sasuke.nome = "Sasuke Uchiha";
+         Sasuke.idade = 18;
+         Sasuke.aldeia = "Aldeia da folha";
+
+         //Criar o Ninja Sakura é um OBEJETO
+
+         Ninja sakura = new Ninja();
+         sakura.nome = "Sakura haruno";
+         sakura.idade = 17;
+         sakura.aldeia = "Aldeia da folha";
+
+
+    }
+}
